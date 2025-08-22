dd4c62099fd713bea86b99540c8d208b3a7fc141
Sneyder <sneyder.marin8119@unaula.edu.co>
2025-08-22 09:33:45 -0500

    Delete Sneyder/Ejercicio_Figuras directory

--- Sneyder/Ejercicio_Figuras/Figura.py
+++
@@ -1,6 +0,0 @@
-class Figura:
-    def __init__(self, nombre):
-        self.nombre = nombre
-
-    def calcular_area_figura(self):
-        raise NotImplementedError("Aca es donde se sobreescribe el metodo por las clases hijas :)")
